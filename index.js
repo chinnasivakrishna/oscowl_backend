eaa2cddaec9c7f33be21d8d5752d2d6237a35593
chinnasivakrishna <139040548+chinnasivakrishna@users.noreply.github.com>
2024-11-13 16:31:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors({
+  origin: ["https://oscowl-frontend-five.vercel.app"],
+  methods: ["GET", "POST","PUT", "DELETE"],
+  credentials:true
+}));
 app.use(authRoutes);
 app.use(profileRoutes);
 app.use(projectRoutes);
